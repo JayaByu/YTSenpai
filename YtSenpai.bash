6ea97fdde7c5a04c2f595ffec481fb274cfdc723
/4l0n3 <70298586+JayaByu@users.noreply.github.com>
2021-06-22 22:55:31 +0700

    Create YtSenpai.bash

---
+++ YtSenpai.bash
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+clear
+
+na="\e[1;36m" 
+red="\e[0;31m" 
+ble="\e[0;34m"
+bld="\e[1;34m"
+nt="\e[0m"
+echo "YTSenpai" | figlet
+echo ""
+echo -e "${red}Youtube Downloader${nt}"
+echo ""
+date
+echo -e "${na}Artemis - Tegalsec${nt} "
+dirvid="Mp4"
+diraud="Mp3"
+echo -e "${bld}
+0. Install Requerement
+1. Mp3
+2. Mp4
+3. Exit${nt}
+ "
+read -p "Artemis@Senpai:#~ " select
+if [[ $select == 0 ]]; 
+    then
+    sudo apt install youtube-dl
+elif [[ $select == 1 ]]; 
+    then
+        read -p "Input Url : " input
+        echo "Wait.. \"$input\""
+        echo "Output aduio in \"$diraud\""
+        echo ""
+
+        Mp3="-o $diraud/%(tittle)s.%(ext)s"
+            echo "Downloading \"$input\""
+            youtube-dl -f 'bestaudio' --ignore-errors --continue --extract-audio --audio-format mp3 $Mp3 $input
+    
+elif [[ $select == 2 ]]; 
+    then
+        read -p "Input Url :" input
+        echo "Wait.. \"$input\""
+        echo "Output aduio in \"$diraud\""
+        echo ""
+        
+        Mp4="-o $dirvid/%(tittle)s.%(ext)s"
+            echo "Downloading \"$input\""
+            youtube-dl --ignore-errors --continue -f mp4 $Mp4 $input
+  
+elif [[ $select == 3 ]];
+    then 
+        echo "Bye!!!"
+fi
